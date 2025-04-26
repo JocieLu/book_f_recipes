60a620015a106c753f4d5c142e3565e3eeeec21e
Ilya Lugovoy <i@ilugovoy.ru>
2025-04-26 22:52:41 +0300

    fix recipe page rename

--- lib/presentation/pages/recipe_list_page.dart
+++ lib/presentation/pages/recipe_list_page.dart
@@ -1,4 +1,4 @@
-import 'package:book_f_recipes/presentation/pages/create_recipe_page.dart';
+import 'package:book_f_recipes/presentation/pages/recipe_create_page.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../core/models/recipe.dart';
